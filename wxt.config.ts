e794b51fe0ae07d25a049e75df3da6ad9208d6f2
tomo <tomohiro.togashi@gmail.com>
2025-04-26 16:30:47 +0900

    バージョン番号を1.1.11から1.1.12に更新 (#11)

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   manifest: {
     name: "Zen Search",
     description: "Search your bookmarks and history",
-    version: "1.1.11",
+    version: "1.1.12",
     icons: {
       16: "icon/16.png",
       32: "icon/32.png",
