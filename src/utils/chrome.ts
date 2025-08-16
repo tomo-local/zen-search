a825349b8f95125fc62cb667001d42bb3677d23c
tomo-local <tomohiro.togashi@gmail.com>
2025-08-16 18:05:26 +0900

    refactor: chrome.tsから未使用のactionQuery関数を削除

--- src/utils/chrome.ts
+++ src/utils/chrome.ts
@@ -26,30 +26,8 @@ const actionPopupContent = async () => {
   await chrome.action.openPopup();
 };
 
-const actionQuery = async (
-  query: string,
-  option: chrome.tabs.QueryInfo
-): Promise<chrome.tabs.Tab[]> => {
-  const response = await chrome.tabs.query(option);
-
-  if (!query) {
-    return response;
-  }
-
-  return response.filter((tab) => {
-    const title = tab.title || "";
-    const url = tab.url ? new URL(tab.url).hostname : "";
-
-    const isTitleMatch = title.toLowerCase().includes(query.toLowerCase());
-    const isUrlMatch = url.toLowerCase().includes(query.toLowerCase());
-
-    return isTitleMatch || isUrlMatch;
-  }) as chrome.tabs.Tab[];
-};
-
 export {
   actionRuntimeContent,
   actionTabsContent,
   actionPopupContent,
-  actionQuery,
 };
