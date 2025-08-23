3c3beb136fa2c9d46c72677b7be7e11a57554d01
tomo <tomohiro.togashi@gmail.com>
2025-08-23 17:20:08 +0900

    feature/add story book (#31)
    * feat: add storybook
    
    * feat: add initial Storybook configuration files
    
    * feat: add privacy policy document for Zen Search extension

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { postcss } from "autoprefixer";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+        viteConfigPath: ".storybook/vite.config.ts",
+      },
+    },
+  },
+};
+export default config;
