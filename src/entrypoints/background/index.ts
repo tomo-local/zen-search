591215da7515af5a99cfac757e1622ad4a554d61
tomo <tomohiro.togashi@gmail.com>
2025-08-23 15:55:41 +0900

    refactor/backgorund structure (#30)
    * refactor: バックグラウンドでのコマンドおよびメッセージのルーティング機能を実装
    
    * feat: mise.tomlを追加し、Node.jsおよびpnpmのバージョンを設定
    
    * chore: .nvmrcファイルを削除

---
+++ src/entrypoints/background/index.ts
@@ -0,0 +1,15 @@
+import { routeMessage, routeCommand } from "./router";
+
+export default defineBackground(() => {
+  /**
+   * @description コマンドのルーティング
+   */
+  chrome.commands.onCommand.addListener((command, tab) => routeCommand(command, tab));
+
+  /**
+   * @description メッセージのルーティング
+   */
+  chrome.runtime.onMessage.addListener((message, sender, response) =>
+    routeMessage(message, sender, response)
+  );
+});
