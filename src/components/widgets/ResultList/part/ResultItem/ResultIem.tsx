ce388cc25638fe4a4868c78ce8561c64f31dee20
tomo-local <tomohiro.togashi@gmail.com>
2025-09-09 00:50:28 +0900

    feat: add ResultItem component with item mapping and mock data

---
+++ src/components/widgets/ResultList/part/ResultItem/ResultIem.tsx
@@ -0,0 +1,48 @@
+import type { Kind, Result } from "@/services/result";
+
+import BookmarkItem from "../../../BookmarkItem/BookmarkItem";
+import HistoryItem from "../../../HistoryItem/HistoryItem";
+import SuggestionItem from "../../../SuggestionItem/SuggestionItem";
+import TabItem from "../../../TabItem/TabItem";
+
+export type ResultItemProps<T extends Kind> = {
+  item: Result<T>;
+  index: number;
+  onClick: () => void;
+  selected: boolean;
+};
+
+const ItemMap: Record<
+  Kind,
+  React.FC<{
+    item: Result<Kind>;
+    index: number;
+    onClick: () => void;
+    selected: boolean;
+  }>
+> = {
+  Tab: (props) => <TabItem {...(props as ResultItemProps<"Tab">)} />,
+  Bookmark: (props) => (
+    <BookmarkItem {...(props as ResultItemProps<"Bookmark">)} />
+  ),
+  History: (props) => (
+    <HistoryItem {...(props as ResultItemProps<"History">)} />
+  ),
+  Suggestion: (props) => (
+    <SuggestionItem {...(props as ResultItemProps<"Suggestion">)} />
+  ),
+};
+
+const ResultItem: React.FC<ResultItemProps<Kind>> = (props) => {
+  const Component = ItemMap[props.item.type];
+  return (
+    <Component
+      item={props.item}
+      index={props.index}
+      onClick={props.onClick}
+      selected={props.selected}
+    />
+  );
+};
+
+export default ResultItem;
