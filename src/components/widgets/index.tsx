bfdb87c93d7484d560fa3c6d6c232c9dbb4688c5
tomo-local <tomohiro.togashi@gmail.com>
2025-09-09 00:03:59 +0900

    feat: add SuggestionItem, HistoryItem, BookmarkItem, and TabItem to widgets index

--- src/components/widgets/index.tsx
+++ src/components/widgets/index.tsx
@@ -1,8 +1,16 @@
+import BookmarkItem, {
+  type BookmarkItemProps,
+} from "./BookmarkItem/BookmarkItem";
+import HistoryItem, { type HistoryItemProps } from "./HistoryItem/HistoryItem";
 import ResultFooter, {
   type ResultFooterProps,
 } from "./ResultFooter/ResultFooter";
 import ResultLine, { type ResultLineProps } from "./ResultLine/ResultLine";
 import SearchInput, { type SearchInputProps } from "./SearchInput/SearchInput";
+import SuggestionItem, {
+  type SuggestionItemProps,
+} from "./SuggestionItem/SuggestionItem";
+import TabItem, { type TabItemProps } from "./TabItem/TabItem";
 import ThemeSelectButton, {
   type ThemeSelectorProps,
 } from "./ThemeSelectButton/ThemeSelectButton";
@@ -12,6 +20,10 @@ export default {
   SearchInput,
   ThemeSelectButton,
   ResultLine,
+  SuggestionItem,
+  HistoryItem,
+  BookmarkItem,
+  TabItem,
 };
 
 export type {
@@ -19,4 +31,8 @@ export type {
   ResultLineProps,
   SearchInputProps,
   ThemeSelectorProps,
+  SuggestionItemProps,
+  HistoryItemProps,
+  BookmarkItemProps,
+  TabItemProps,
 };
