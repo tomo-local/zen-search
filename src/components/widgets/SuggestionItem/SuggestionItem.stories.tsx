a677bc7c7f95c7208633c800b304257592ddf3df
tomo-local <tomohiro.togashi@gmail.com>
2025-09-08 23:49:36 +0900

    feat: add SuggestionItem component with customizable props and default styles

---
+++ src/components/widgets/SuggestionItem/SuggestionItem.stories.tsx
@@ -0,0 +1,26 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SuggestionItem, { type SuggestionItemProps } from './SuggestionItem';
+
+const meta: Meta = {
+  component: SuggestionItem,
+};
+
+export default meta;
+
+type Story = StoryObj<SuggestionItemProps>;
+
+export const Default: Story = {
+  args: {
+    item: {
+      data: {
+        title: "Example Suggestion",
+        url: "https://example.com",
+        type: "Google",
+      },
+    },
+    onClick: action('SuggestionItem clicked'),
+    selected: false,
+  },
+};
