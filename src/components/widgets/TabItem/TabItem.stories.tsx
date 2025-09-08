3482655bb876552305d7130297f2fa25c5c7de80
tomo-local <tomohiro.togashi@gmail.com>
2025-09-08 23:07:13 +0900

    feat: add TabItem component with customizable props and default styles

---
+++ src/components/widgets/TabItem/TabItem.stories.tsx
@@ -0,0 +1,27 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import TabItem, { type TabItemProps } from "./TabItem";
+
+const meta: Meta = {
+  component: TabItem,
+};
+
+export default meta;
+
+type Story = StoryObj<TabItemProps>;
+
+export const Default: Story = {
+  args: {
+    item: {
+      data: {
+        id: 1,
+        title: "あ\nあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ",
+        url: "https://example.com",
+        favIconUrl: "https://example.com/favicon.ico",
+      },
+    },
+    onClick: action("onClick"),
+    selected: false,
+  },
+};
---
+++ src/components/widgets/TabItem/TabItem.tsx
@@ -0,0 +1,67 @@
+import ArrowLongRightIcon from "@heroicons/react/16/solid/ArrowLongRightIcon";
+import WindowIcon from "@heroicons/react/16/solid/WindowIcon";
+import clsx from "clsx";
+import type { NewTab as Tab } from "@/services/tab/types";
+
+import ButtonItem, {
+  type ButtonItemProps,
+  defaultClassName,
+} from "../../modules/ButtonItem/ButtonItem";
+import SquareIcon from "../../modules/SquareIcon/SquareIcon";
+
+type TabData = Pick<Tab["data"], "id" | "title" | "url" | "favIconUrl">;
+
+export type TabItemProps = Pick<ButtonItemProps, "onClick" | "selected"> & {
+  item: {
+    data: TabData;
+  };
+};
+
+const TabItem: React.FC<TabItemProps> = ({ item, onClick, selected }) => {
+  const LeftIcon = (
+    <SquareIcon>
+      <WindowIcon
+        className={clsx(defaultClassName.icon.size, defaultClassName.icon.text)}
+      />
+    </SquareIcon>
+  );
+
+  const RightContent = (
+    <div className="flex items-center space-x-2">
+      <span className={defaultClassName.text}>Go to Tab</span>
+      <SquareIcon className={clsx(selected && defaultClassName.icon.bg)}>
+        <ArrowLongRightIcon
+          className={clsx(
+            defaultClassName.icon.size,
+            selected && defaultClassName.icon.selected
+          )}
+        />
+      </SquareIcon>
+    </div>
+  );
+
+  return (
+    <ButtonItem
+      className={clsx(
+        defaultClassName.bg,
+        defaultClassName.border,
+        defaultClassName.hover
+      )}
+      onClick={onClick}
+      selected={selected}
+      LeftContent={LeftIcon}
+      RightContent={RightContent}
+    >
+      <div
+        id={item.data.id?.toString()}
+        className="relative flex-col items-center justify-center max-w-fit justify-items-center"
+      >
+        <div className="text-sm truncate max-w-[224px] md:max-w-md whitespace-nowrap">
+          {item.data.title}
+        </div>
+      </div>
+    </ButtonItem>
+  );
+};
+
+export default TabItem;
