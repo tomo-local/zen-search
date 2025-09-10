aa19117d8453a8b7149a6d0811bb97b2066edf7b
tomo-local <tomohiro.togashi@gmail.com>
2025-09-11 00:00:26 +0900

    feat: update ButtonItem styles and add disabled prop

--- src/components/modules/ButtonItem/ButtonItem.tsx
+++ src/components/modules/ButtonItem/ButtonItem.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 
 export interface ButtonItemProps {
   className?: string | undefined;
+  disabled?: boolean;
   onClick?: (event: React.MouseEvent) => void;
   children: React.ReactNode;
   LeftContent?: React.ReactNode;
@@ -13,16 +14,16 @@ export const defaultClassName = {
   base: "flex items-center justify-between w-full px-4 py-2 text-left rounded-lg",
   bg: "dark:bg-gray-800 bg-gray-50",
   border: "border-sky-500",
-  selected: "dark:bg-sky-500 bg-sky-400 dark:text-black text-white",
+  selected: "dark:bg-sky-600 bg-sky-400 dark:text-gray-300 text-gray-100",
   hover:
     "hover:dark:bg-sky-700 hover:bg-sky-300 hover:opacity-80 hover:cursor-pointer",
   icon: {
-    bg: "dark:bg-gray-300 bg-slate-200",
+    bg: "dark:bg-sky-200 bg-sky-50",
     text: "dark:text-gray-300",
-    selected: "dark:text-gray-500 text-sky-500",
+    selected: "dark:text-sky-500 text-sky-400",
     size: "size-5",
   },
-  text: "dark:text-gray-300 text-xs",
+  text: "dark:text-gray-300 text-xs font-medium",
 };
 
 const ButtonItem: React.FC<ButtonItemProps> = (props) => {
@@ -30,8 +31,10 @@ const ButtonItem: React.FC<ButtonItemProps> = (props) => {
     <li className="list-none">
       <button
         type="button"
+        disabled={props.disabled}
         className={clsx(
           defaultClassName.base,
+          defaultClassName.text,
           props.selected && defaultClassName.selected,
           props.className,
         )}
