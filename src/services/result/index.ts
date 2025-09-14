7e4175452bb2d5fc349b4472035a5c9cc8a510f9
tomo-local <tomohiro.togashi@gmail.com>
2025-09-14 22:39:04 +0900

    feature: remove unused converter export from result service index

--- src/services/result/index.ts
+++ src/services/result/index.ts
@@ -2,7 +2,6 @@
  * Result Service - エクスポート用インデックス
  */
 
-export * from "./converter";
 export * from "./helper";
 export { type ResultService, resultService } from "./service";
 export type * from "./types";
