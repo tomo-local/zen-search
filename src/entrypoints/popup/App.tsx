f74e6449f9a500d4bb7034f93c782ffde0dcc243
tomo <tomohiro.togashi@gmail.com>
2025-09-23 01:57:57 +0900

    feature/add use debounce query (#96)
    * feat: add useDebounce hook for value debouncing functionality
    
    * feat: implement debouncing for query input in App component

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -14,6 +14,7 @@ import SearchInput, {
   commonClassName as searchInputClassName,
 } from "@/components/widgets/SearchInput/SearchInput";
 import useArrowKeyControl from "@/hooks/useArrowKeyControl";
+import useDebounce from "@/hooks/useDebounce";
 import useEnterKeyControl from "@/hooks/useEnterKeyControl";
 import usePopupShortcut from "@/hooks/usePopupShortcut";
 import useQueryControl from "@/hooks/useQueryControl";
@@ -28,8 +29,10 @@ export default function App() {
     reset,
   } = useQueryControl();
 
+  const debouncedQuery = useDebounce(query, 200);
+
   const { results, loading: resultsLoading } = useResult({
-    query,
+    query: debouncedQuery,
     categories,
   });
 
