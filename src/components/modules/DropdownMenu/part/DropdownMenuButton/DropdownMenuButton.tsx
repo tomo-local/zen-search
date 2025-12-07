635474345e7d626a1acc3b9351b57cbb114c10ed
tomo-local <tomohiro.togashi@gmail.com>
2025-12-07 17:15:30 +0900

    DropdownMenuコンポーネントとそのサブコンポーネントを追加し、ストーリーを作成

---
+++ src/components/modules/DropdownMenu/part/DropdownMenuButton/DropdownMenuButton.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from "react";
+import { useDropdownMenu } from "../../hooks";
+
+export interface DropdownMenuButtonProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export const DropdownMenuButton: React.FC<DropdownMenuButtonProps> = ({
+  className,
+  children,
+}) => {
+  const { onToggle, isOpen } = useDropdownMenu();
+
+  return (
+    <button
+      type="button"
+      onClick={onToggle}
+      className={className}
+      aria-expanded={isOpen}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ src/components/modules/DropdownMenu/part/index.tsx
@@ -0,0 +1,20 @@
+import {
+  DropdownMenuButton,
+  type DropdownMenuButtonProps,
+} from "./DropdownMenuButton/DropdownMenuButton";
+import {
+  DropdownMenuItem,
+  type DropdownMenuItemProps,
+} from "./DropdownMenuItem/DropdownMenuItem";
+import {
+  DropdownMenuItems,
+  type DropdownMenuItemsProps,
+} from "./DropdownMenuItems/DropdownMenuItems";
+
+export { DropdownMenuButton, DropdownMenuItems, DropdownMenuItem };
+
+export type {
+  DropdownMenuButtonProps,
+  DropdownMenuItemsProps,
+  DropdownMenuItemProps,
+};
