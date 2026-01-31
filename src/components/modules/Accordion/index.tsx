62dcbd7ab2e70c5729168217b29ea4a171770a43
tomo <tomohiro.togashi@gmail.com>
2026-02-01 00:58:42 +0900

    Chore/organiz (#116)
    * Feature: プライバシーポリシーとREADMEを追加
    
    * Feature: Remove Modal and Accordion components along with their stories and hooks

--- src/components/modules/Accordion/index.tsx
+++
@@ -1,6 +0,0 @@
-import Accordion, { type AccordionProps } from "./Accordion";
-import type { AccordionContentProps, AccordionTitleProps } from "./part";
-import { AccordionContent, AccordionTitle } from "./part";
-
-export type { AccordionTitleProps, AccordionContentProps, AccordionProps };
-export { AccordionContent, AccordionTitle, Accordion };
